1a0f31ff5c66f9620dd15a0d7f22388fa7cb68fd
Mark Q Schwanda <mqschwanda@gmail.com>
2022-04-08 16:45:29 -0400

    refactor(ui): ui provider

--- apps/nextjs-app/src/app-providers.tsx
+++ apps/nextjs-app/src/app-providers.tsx
@@ -1,7 +1,6 @@
 import type { EmotionCache } from '@emotion/react';
-import { CacheProvider } from '@emotion/react';
-import { ThemeProvider as MuiThemeProvider } from '@mui/material';
-import type { FC, ReactNode } from 'react';
+import { UIProvider } from '@mqs/ui-lib';
+import type { FC } from 'react';
 
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { createEmotionCache } from '@/lib/emotion';
@@ -26,14 +25,10 @@ type Props = {
 export const AppProviders: FC<Props> = (props) => {
   const { emotionCache = clientSideEmotionCache } = props;
   return (
-    <CacheProvider value={emotionCache}>
-      <MuiThemeProvider theme={muiTheme}>
-        {/* Mui CssBaseline disabled in this example as tailwind provides its own */}
-        {/* <CssBaseline /> */}
-        <QueryClientProvider client={queryClient}>
-          {props.children}
-        </QueryClientProvider>
-      </MuiThemeProvider>
-    </CacheProvider>
+    <UIProvider cache={emotionCache} theme={muiTheme}>
+      <QueryClientProvider client={queryClient}>
+        {props.children}
+      </QueryClientProvider>
+    </UIProvider>
   );
 };
