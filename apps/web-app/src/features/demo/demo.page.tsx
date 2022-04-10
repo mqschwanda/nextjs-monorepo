a147c4b6bd91e3fa0f2c3111d9feda2201abdd78
Mark Q Schwanda <mqschwanda@gmail.com>
2022-04-09 22:49:03 -0400

    refactor(web-app): layouts and partials

--- apps/web-app/src/features/demo/demo.page.tsx
+++ apps/web-app/src/features/demo/demo.page.tsx
@@ -1,8 +1,7 @@
 import { useTranslation } from 'next-i18next';
 import { NextSeo } from 'next-seo';
 import type { FC } from 'react';
-import { Banner } from '@/components/banner';
-import { MainLayout } from '@/components/layout/main-layout';
+import { MainLayout } from '@/layouts/MainLayout';
 import { demoConfig } from './demo.config';
 import { DemoApiSection } from './sections';
 
@@ -16,7 +15,6 @@ export const DemoPage: FC = () => {
         description="Web-app nextjs monorepo example, https://github.com/mqschwanda/nextjs-monorepo"
       />
       <MainLayout>
-        <Banner />
         <DemoApiSection />
       </MainLayout>
     </>
--- apps/web-app/src/features/home/home.page.tsx
+++ apps/web-app/src/features/home/home.page.tsx
@@ -1,19 +1,15 @@
 import { useTranslation } from 'next-i18next';
 import { NextSeo } from 'next-seo';
 import type { FC } from 'react';
-<<<<<<< HEAD:apps/nextjs-app/src/features/home/pages/HomePage.tsx
-import { Banner } from '@/components/Banner';
-import { MainLayout } from '@/components/layout/MainLayout';
-import { CtaBlock, FeaturesBlock, HeroBlock } from '../blocks';
-import { homeConfig } from '../home.config';
-=======
-import { Banner } from '@/components/banner';
-import { MainLayout } from '@/components/layout/main-layout';
+import { MainLayout } from '@/layouts/MainLayout';
 import { homeConfig } from './home.config';
 import { CtaSection, FeaturesSection, HeroSection } from './sections';
->>>>>>> 1593ce10 (refactor(web-app): sections):apps/web-app/src/features/home/home.page.tsx
 
-export const HomePage: FC = () => {
+type Props = {
+  children?: never;
+};
+
+export const HomePage: FC<Props> = () => {
   const { t } = useTranslation(homeConfig.i18nNamespaces);
 
   return (
@@ -23,7 +19,6 @@ export const HomePage: FC = () => {
         description="See https://github.com/mqschwanda/nextjs-monorepo"
       />
       <MainLayout>
-        <Banner />
         <HeroSection />
         <FeaturesSection />
         <CtaSection />
