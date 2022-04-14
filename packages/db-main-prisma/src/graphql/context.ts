68cb90b17ca25dae350836b864c818dab10d72f4
Mark Q Schwanda <mqschwanda@gmail.com>
2022-04-13 23:39:41 -0400

    refactor(graphql): prisma and graphql

--- packages/db-main-prisma/src/graphql/context.ts
+++ packages/db-main-prisma/src/graphql/context.ts
@@ -1,10 +1,49 @@
-import type { PrismaClient } from '@prisma/client';
-import { getPrismaClient } from '../prisma/client';
+import { PrismaClient } from '@prisma/client';
+import packageJSON from '../../package.json';
+import { PrismaManager } from '../prisma';
+
+const isDevelopment = process.env?.NODE_ENV === 'development';
 
 export type GraphqlContext = {
   prisma: PrismaClient;
 };
 
 export const context: GraphqlContext = {
-  prisma: getPrismaClient(),
+  prisma: PrismaManager.getDevSafeInstance(packageJSON.name, () => {
+    const client = new PrismaClient({
+      datasources: {
+        db: {
+          url: process.env?.PRISMA_DATABASE_URL,
+        },
+      },
+      errorFormat: isDevelopment ? 'pretty' : 'colorless',
+      log: [
+        {
+          level: 'query',
+          emit: 'event',
+        },
+        {
+          level: 'error',
+          emit: 'stdout',
+        },
+        {
+          level: 'info',
+          emit: 'stdout',
+        },
+        {
+          level: 'warn',
+          emit: 'stdout',
+        },
+      ],
+    });
+
+    if (isDevelopment) {
+      client.$on('query', (e) => {
+        console.log('Query: ' + e.query);
+        console.log('Duration: ' + e.duration + 'ms');
+      });
+    }
+
+    return client;
+  }),
 };
