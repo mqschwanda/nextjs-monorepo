646a500bfe2b8aa49931b9e7f0378b47a54bdd39
Mark Q Schwanda <mqschwanda@gmail.com>
2022-04-15 16:48:46 -0400

    refactor(eslint): move eslint config to package

--- packages/db-main-prisma/.eslintrc.js
+++ packages/db-main-prisma/.eslintrc.js
@@ -5,11 +5,5 @@
 
 module.exports = {
   root: true,
-  ignorePatterns: ['dist', 'build', 'src/generated'],
-  extends: ['../../.eslintrc.base.js'],
-  env: {
-    browser: false,
-    es6: true,
-    node: true,
-  },
+  extends: ['plugin:@mqs/eslint-plugin/base'],
 };
---
+++ packages/eslint-plugin/src/index.js
@@ -0,0 +1,11 @@
+const base = require('./configs/base');
+const next = require('./configs/next');
+const react = require('./configs/react');
+
+module.exports = {
+  configs: {
+    base,
+    next,
+    react,
+  },
+};
